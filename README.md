411ec090ad07b57e83622b379770a83435e06d15
se1ob <radoslav.kontur@erstegroup.com>
2022-04-20 17:19:11 +0200

    Empty project

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # Word count
 
-You need [Node.js](https://nodejs.org) version 12 or later to run this.
-
 To run:
 
-    $ node .
+    mvn clean test
---
+++ src/main/java/com/erstegroup/app/App.java
@@ -0,0 +1,9 @@
+package com.erstegroup.app;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Word Count!" );
+    }
+}
